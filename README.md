b426693af9591d534e056734da47be2158dbc384
Sumaid <sumaidsyed@gmail.com>
2018-09-26 23:05:55 +0530

    script added

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # moviealarm
 
-An alarm system for movie ticket booking. Many times we don't get the chance to book the tickets as soon as they arrive, so this script provides a notification system for that.User just need to specify the movie name in script to get alarms.
+An alarm system for movie ticket booking. Many times we don't get the chance to book the tickets as soon as they arrive, so this script provides a notification system for that.User just need to specify the movie name in script to get 
+1)E-mail notification
+2)Desktop Alert
+3)Laptop Sound Alarm
+The script is written in python 3
+
+Requirements(Python Packages/Modules):
+	email.mime.multipart
+	email.mime.text
+	requests
+	bs4
+	smtplib
+	gi.repository
+
+Instructions:
+	run python3 script.py &
+	(Laptop needs to be open in the duration)
---
+++ script.py
@@ -0,0 +1,44 @@
+    #!/usr/bin/env python3
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+import subprocess
+import time
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from gi.repository import Notify
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("Gmail account", "Pass word")
+msg = MIMEText("""Dear reciever,
+  If you're reading this then it means Infinity war tickets are out!!!!!This is an automated mail...I sent this on 24th April 1:30 pm...If I'm still alive contact me somehow and make me book tickets and if I'm not alive then you book tickets and enjoy<3....Fast! You don't have much time.............
+  Yours sincerely,
+  Sumaid Syed(of past)""")
+sender = "sumaidsyed@gmail.com"
+recipients = ['sumaidsyed@gmail.com', 'syed.sumaid@gmail.com', 'sumaid.ali@students.iiit.ac.in', 'sanjana99sunil@gmail.com', 'mehershashwat@gmail.com', 'shantanu.das@research.iiit.ac.in', 'n.sriven@gmail.com']
+msg['Subject'] = "Infinity War Tickets out....Tell Me"
+msg['From'] = sender
+msg['To'] = ", ".join(recipients)
+count = 0
+
+while (count!=1):
+  res = requests.get('https://in.bookmyshow.com/buytickets/avengers-infinity-war-hyderabad/movie-hyd-ET00053419-MT/20180427')
+  print(res.status_code)
+
+  text_data = res.text
+  soup = BeautifulSoup(text_data)
+
+  #dude = soup.select('a')
+  dude = soup.find_all( attrs={"data-name" : "Prasads: Large Screen"} )
+  count = len(dude)  
+  if len(dude)==1:
+      print("Book your ticket! Book your ticket!")
+      Notify.init("start")
+      notification = Notify.Notification.new("Book your ticket, Book your ticket!")
+      notification.set_app_name("BvsS")
+      notification.show()
+      s.sendmail(sender, recipients, msg.as_string())
+      subprocess.call(['play', 'siren2.wav'])
+      break
+  time.sleep(1)
